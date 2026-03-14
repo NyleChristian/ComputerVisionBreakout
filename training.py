58e91cdd997d73db7fd11e2e5b5cd3ad17430957
NyleChristian <nylopop@gmail.com>
2026-03-14 17:24:59 -0400

    yolo11 large model run

---
+++ training.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+def main():
+
+    model = YOLO("yolo11l.pt")
+
+    results = model.train(
+        data = "data.yaml",
+        epochs=100,
+        imgsz=1000, 
+        batch=-1,
+        patience=20,
+        device=0,
+        
+    )
+
+if __name__ == '__main__':
+    main()
